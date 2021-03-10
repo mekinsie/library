50a0e8b24ed74e0e290839a65fc112a75a512dd7
Mekinsie Callahan <mekinsie.aja@gmail.com>
2021-03-10 10:28:21 -0800

    Pass first test.
    Co-authored-by: Jon Stump <jmstump@gmail.com>

--- lib/book.rb
+++ lib/book.rb
@@ -1,17 +1,27 @@
 class Book
 
-attr_accessor :title, :genre, :publish_date, :check_out
+attr_accessor :title, :genre, :publish_date, :checkout
 attr_reader :id
 
 def intitialize(attributes)
   @title = attributes.fetch(:title)
   @genre = attributes.fetch(:genre)
-  @publish_date = attributes.fetch(:publish_date)
-  @checkout = attributes.fetch(:check_out)
+  @pub_date = attributes.fetch(:publish_date)
+  @checkout = attributes.fetch(:checkout)
 end
 
-def self.all 
-
+def self.all
+  books = []
+  returned_books = DB.exec("SELECT * FROM books;")
+  returned_books.each do |book|
+    title = book.fetch('title')
+    genre = book.fetch('genre')
+    pub_date = book.fetch('publish_date')
+    checkout = book.fetch('checkout')
+    id = book.fetch('id').to_i
+    books << Book.new({title: title, genre: genre, publish_date: pub_date, checkout: checkout, id: id})
+  end
+  books
 end
 
 end
