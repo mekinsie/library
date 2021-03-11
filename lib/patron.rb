ccc6af2b08b5b3f86150f35a3f486d03bd61e0db
Jon Stump <jmstump@gmail.com>
2021-03-11 11:47:24 -0800

    add all method for patrons
    Co-authored-by: Mekinsie Callahan <mekinsie.aja@gmail.com>

--- lib/patron.rb
+++ lib/patron.rb
@@ -1,4 +1,23 @@
 class Patron
+  attr_accessor :first_name, :last_name
+  attr_reader :id
 
+  def initialize(attributes)
+    @first_name = attributes[:first_name]
+    @last_name = attributes[:last_name]
+    @id = attributes[:id]
+  end
+
+  def self.all
+    patrons = []
+    returned_patrons = DB.exec("SELECT * FROM patrons;")
+    returned_patrons.each do |patron|
+      first_name = patron.fetch('first_name')
+      last_name = patron.fetch('last_name')
+      id = patron.fetch('id').to_i
+      patrons << Patron.new({first_name: first_name, last_name: last_name, id: id})
+    end
+    patrons
+  end
 
 end
--- spec/patron_spec.rb
+++ spec/patron_spec.rb
@@ -1,1 +1,12 @@
 require 'spec_helper'
+
+describe '#Patron' do
+
+
+  describe('.all') do
+    it("returns an empty array when there are no patrons") do
+      expect(Patron.all).to(eq([]))
+    end
+  end
+
+end
